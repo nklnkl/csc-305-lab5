37bb95ed22562d2884a12438969682d27f6a9d16
niko lagman <lagman.niko@gmail.com>
2018-12-23 10:33:02 -0500

    Compile apps

---
+++ app.sh
@@ -0,0 +1,18 @@
+# CSC-305 Lab 5 Shell
+# Niko Lagman - Fall 2018
+echo "CSC-305 Lab 5 Shell"
+echo "Niko Lagman - Fall 2018"
+
+ps
+
+echo ""
+echo "App 1"
+time ./app1
+
+echo ""
+echo "App 2"
+time ./app2
+
+echo ""
+echo "App 3"
+time ./app3
